c6310e6ec69b5537bbf1ee8e571f82d882bbcfa8
Eric Hasselbring <ehasselbring@gmail.com>
2016-11-15 22:17:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
 
+    if array.length == 1
+        return array.join
+    end
+
+    if array.length == 2
+        return array.join(" and ")
+    end
+
+    if array.length === 3
+        new_array = []
+        new_array.push(array.shift(2))
+        return new_array.join(", ")+", and " + array[-1]
+    end
+
+    if array.length > 3
+        new_array = []
+        new_array.push(array.shift(array.length - 1))
+        return new_array.join(", ")+", and " + array[-1]
+    end
+
+
 end
